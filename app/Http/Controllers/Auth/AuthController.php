de23193d2cd7935a0d8aa46ca1a582b113eac31b
ValentinaAV <valentina.alvarez.vallejo@gmail.com>
2016-10-13 15:11:21 -0500

    Creación de login y redireccionamiento
    Se implemento las siguientes funcionalidades: login, redireccionamiento
    de rutas, registro de usuarios, controlador de menú de inicio y el
    módulo de autenticación.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -8,6 +8,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Http\Request;
+use Sessions;
+
 class AuthController extends Controller
 {
     /*
@@ -24,21 +28,91 @@ class AuthController extends Controller
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
     /**
-     * Where to redirect users after login / registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/';
-
-    /**
      * Create a new authentication controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Guard $auth)
     {
-        $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+        $this->auth = $auth;
+        $this->middleware('guest',['except' => 'getLogout']);
     }
+
+   /**
+     * Create a new authentication controller instance.
+     *
+     * @return void
+     */
+    public function getLogin()
+    {
+        return view('login');
+    }
+
+   /**
+     * Create a new authentication controller instance.
+     *
+     * @return void
+     */
+    public function postLogin(Request $request)
+    {
+        $this->validate($request,['email'=> 'required', 'password'=>'required',]);
+
+        $credentials = $request->only('email','password');
+        if($this->auth->attempt($credentials,$request->has('remember'))){
+            return view('home');
+        }
+
+        return view()->with("msjerror","Credenciales Incorrectas");
+    }
+
+        /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function getRegister()
+    {
+        return view('registro');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function postRegister(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',]);
+
+        $data = $request;
+        $user = new User;
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->password = bcrypt($data['password']);
+
+        if($user->save()){
+            return "Se ha registrado correctaente el USUARIO";
+        }
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function getLogout()
+    {
+        $this->auth->logout();
+        Sessions::flush();
+        return redirect('login');
+    }
+
 
     /**
      * Get a validator for an incoming registration request.
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,8 +9,25 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-*/
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+
+Route::get('home', function () {
+    return view('home');
+});
+
+
+*/
+
+Route::get('login','Auth\AuthController@getLogin');
+Route::post('login',['as' => 'login', 'uses' => 'Auth\AuthController@postLogin']);
+Route::get('logout',['as' => 'logout', 'uses' => 'Auth\AuthController@getLogout']);
+
+//Registrar Routes
+Route::get('register','Auth\AuthController@getRegister');
+Route::post('register',['as' => 'auth\register', 'uses' => 'Auth\AuthController@postRegister']);
+Route::get('/','HomeController@index');
+Route::get('home','HomeController@index');
